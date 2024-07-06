35c8d62e1461dc84d976f1549d1105d7ab2ebe5a
aliciakimani.ky@gmail.com <aliciakimani.ky@gmail.com>
2024-07-06 13:10:14 +0300

    added a part to calculate the proof

--- blockchain.py
+++ blockchain.py
@@ -10,6 +10,28 @@ class Blockchain:
                                            )
         self.chain.append(genesis_block)
 
+    def mine_block(self, data:str) -> dict:
+        previous_block = self.get_previous_block()
+        previous_proof = previous_block["proof"]
+        index = len(self.chain) + 1
+        proof = None
+
+    def _to_digest(self, new_proof: int, previous_proof: int, index: str, data: str
+                   ) -> bytes:
+     to_digest = str(new_proof ** 2 - previous_proof ** 2 + index)  + data
+
+     def _proof_of_work(self, previous_proof: str, index: int, data:str) -> int:
+            new_proof = 1
+            check_proof = False
+
+            while not check_proof:
+                to_digest = None
+            pass
+       
+
+    def get_previous_block(self) -> dict:
+        return self.chain[-1]
+
     def _create_block(self, data: str, proof: int, previous_hash: str, index:int) -> dict:
        block = {
            "index": index,
